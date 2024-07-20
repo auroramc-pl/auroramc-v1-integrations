62bf1bbbc5fde2a0f7f582c7debd24127f5f8cc5
Rafał Chomczyk <hello@rafal.moe>
2024-07-20 21:11:49 +0200

    Add an dsl for recently introduced rewards

---
+++ auroramc-integrations-platforms/auroramc-integrations-platform-bukkit/src/main/java/pl/auroramc/integrations/reward/RewardsDsl.java
@@ -0,0 +1,45 @@
+package pl.auroramc.integrations.reward;
+
+import static groovy.lang.Closure.DELEGATE_ONLY;
+import static org.bukkit.Bukkit.getServer;
+import static org.bukkit.Material.STONE;
+
+import groovy.lang.Closure;
+import groovy.lang.DelegatesTo;
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.inventory.ItemStack;
+import pl.auroramc.integrations.item.ItemStackBuilder;
+
+public class RewardsDsl {
+
+  private final List<Reward> rewards;
+
+  RewardsDsl() {
+    this.rewards = new ArrayList<>();
+  }
+
+  public void item(final ItemStack item) {
+    rewards.add(new ItemQuestReward(item));
+  }
+
+  public void item(final @DelegatesTo(ItemStackBuilder.class) Closure<?> closure) {
+    final ItemStackBuilder delegate = ItemStackBuilder.newBuilder(STONE);
+    closure.setDelegate(delegate);
+    closure.setResolveStrategy(DELEGATE_ONLY);
+    closure.call();
+    rewards.add(new ItemQuestReward(delegate.build()));
+  }
+
+  public void exec(final List<String> commands) {
+    rewards.add(new ExecQuestReward(getServer(), commands));
+  }
+
+  public void exec(final String... commands) {
+    exec(List.of(commands));
+  }
+
+  public List<Reward> rewards() {
+    return rewards;
+  }
+}
